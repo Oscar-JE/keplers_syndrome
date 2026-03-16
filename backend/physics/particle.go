2612b8b7b178b13c9e101180e0105f365c22fd63
Oscar Johannsson Elling <oscar.johansson.elling@gmail.com>
2026-03-16 07:27:40 +0100

    byte av dator

--- backend/physics/particle.go
+++ backend/physics/particle.go
@@ -10,4 +10,20 @@ type kinematic struct {
 type Particle struct {
 	movement kinematic
 	mass float64
+	radia float64
+}
+
+func dualCollision(p1 Particle, p2 Particle, timeStep float64) (Particle , Particle) {
+	// börjar med att kolla om de kolliderar
+	// hitta den nädmaste punkten och kolla om det är mer eller mindre än
+	// den kombinerade radien av de båda partiklarna
+	// kan jag göra det så enkel??
+	// får jag tekniska problem om de korsar varandra i tidsspannet?
+
+	// om man kollar på vektorn mellan de två vektorerna så kan man använda intervall halvering
+	// för att hitta punkten som de är närmast varandra,
+	// notera att vektorn mellan dem roterar samtidigt som partiklarna rör sig mot eller ifrån varandra
+	//
+
+	//
 }

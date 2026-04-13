484460a0b708a2896cea6a65bee74696dc88a77a
viojog <oscar.johanssonelling@vitecsoftware.com>
2026-04-13 16:11:21 +0200

    pilli pill

--- backend/physics/particle.go
+++ backend/physics/particle.go
@@ -22,20 +22,28 @@ func kollisionDetailjsFromParticle(p Particle) kollisionDetailjs {
 	return kollisionDetailjs{velocity: p.movement.velocity, mass: p.mass}
 }
 
-func timeToCollision(pos1 vec.Vec, v1 vec.Vec, pos2 vec.Vec, v2 vec.Vec, distance float64) float64 {
+func timeToCollision(pos1 vec.Vec, v1 vec.Vec, pos2 vec.Vec, v2 vec.Vec, distance float64) (bool, float64) {
 	deltaPos := vec.Subtract(pos2, pos1)
 	deltaV := vec.Subtract(v2, v1)
 	if vec.DotProduct(deltaPos, deltaPos) < distance*distance {
-		return 0
+		return true, 0
 	}
-	closestPoint := closestPoint(deltaPos, deltaV)
+	closestPoint := closestPointToZero(deltaPos, deltaV)
+	// börja med att kolla om de koliderar
+	//borde vara att kolla om det närmaste avståndet är
 
-	return vec.DotProduct(closestPoint, closestPoint) // vet att detta är fel
+	if vec.DotProduct(closestPoint, closestPoint) > distance*distance { // det blir ingen kollision
+		return false, -1
+	} else {
+
+	}
+
+	return true, vec.DotProduct(closestPoint, closestPoint)
 }
 
-func closestPoint(deltaPos vec.Vec, deltaV vec.Vec) vec.Vec {
-	fromClosestPoint := vec.ScalarMultiplication(vec.DotProduct(deltaV, deltaPos)/vec.DotProduct(deltaV, deltaV), deltaV)
-	return vec.Subtract(deltaPos, fromClosestPoint)
+func closestPointToZero(startPosition vec.Vec, velocity vec.Vec) vec.Vec {
+	fromClosestPoint := vec.ScalarMultiplication(vec.DotProduct(velocity, startPosition)/vec.DotProduct(velocity, velocity), velocity)
+	return vec.Subtract(startPosition, fromClosestPoint)
 }
 
 func dualCollision(p1 Particle, p2 Particle, timeStep float64) (Particle, Particle) {
--- backend/physics/particle_test.go
+++ backend/physics/particle_test.go
@@ -13,6 +13,31 @@ func TestFrontalKollision(t *testing.T) {
 	fmt.Println(v2)
 }
 
-func TestClosestPoint(t *testing.T) {
-	t.Skip("skriv test för att motbevisa hitta closest point")
+func TestClosestPointEnterOrigo(t *testing.T) {
+	start := vec.Init([]float64{-5, 0})
+	velocity := vec.Init([]float64{1, 0})
+	closest := closestPointToZero(start, velocity)
+	if !vec.Eq(closest, vec.Zero(2)) {
+		t.Errorf("this line should clearly pass through origo")
+	}
+}
+
+func TestClosestPointAboveOrigo(t *testing.T) {
+	start := vec.Init([]float64{-5, 5})
+	velocity := vec.Init([]float64{1, 0})
+	closest := closestPointToZero(start, velocity)
+	expected := vec.Init([]float64{0, 5})
+	if !vec.Eq(closest, expected) {
+		t.Errorf("this line should clearly pass through origo")
+	}
+}
+
+func TestClosestPointTwisted(t *testing.T) {
+	start := vec.Init([]float64{-2, 0})
+	velocity := vec.Init([]float64{1, 1})
+	closest := closestPointToZero(start, velocity)
+	expected := vec.Init([]float64{-1, 1})
+	if !vec.Eq(closest, expected) {
+		t.Errorf("Inspect case closestPointTwisted")
+	}
 }
